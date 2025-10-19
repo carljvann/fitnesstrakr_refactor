20903a9400cf3ea4eaf0204c30e2ca1054d68aa3
Carl Vann <carlj.vann@gmail.com>
2025-10-19 10:57:20 -0400

    changes

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
-/** A form that allows users to register for a new account */
+/** Register for a new account */
 export default function Register() {
   const { register } = useAuth();
   const navigate = useNavigate();
